7437dd0c5baed25d854fda350c7e0972d55aa40a
shabbirshakir <70955118+shabbirshakir@users.noreply.github.com>
2020-09-08 06:08:33 -0700

    created size .c
    this file prints size of int,float,double and char in system

---
+++ size.c
@@ -0,0 +1,11 @@
+#inlcude<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+  cout<<"size of int is"<<sifeof(int)<<"bytes<<endl;
+  cout<<"size of float is"<<sifeof(float)<<"bytes<<endl;
+  cout<<"size of double is"<<sifeof(double)<<"bytes<<endl;
+  cout<<"size of char is"<<sifeof(char)<<"bytes<<endl;
+  return 0;
+ }
